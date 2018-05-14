bcbbede28a66b612f1bb91e7ee46fccb241dde40
Jamie Cruwys <me@jamiecruwys.co.uk>
2018-05-15 00:48:41 +0100

    Three apples should be price of two due to bogof offer

--- app/src/test/java/uk/co/jamiecruwys/cart/DiscountedBasketUnitTest.java
+++ app/src/test/java/uk/co/jamiecruwys/cart/DiscountedBasketUnitTest.java
@@ -34,4 +34,15 @@ public class DiscountedBasketUnitTest {
         BigDecimal actual = basket.getTotal();
         assertEquals(new BigDecimal(0.60).setScale(2, RoundingMode.HALF_UP), actual);
     }
+
+    @Test
+    public void three_apples_should_be_price_of_two_due_to_bogof() {
+        Basket basket = new Basket();
+        basket.add(new ApplesBuyOneGetOneFreeOffer());
+        basket.add(new Apple());
+        basket.add(new Apple());
+        basket.add(new Apple());
+        BigDecimal actual = basket.getTotal();
+        assertEquals(new BigDecimal(1.20).setScale(2, RoundingMode.HALF_UP), actual);
+    }
 }
