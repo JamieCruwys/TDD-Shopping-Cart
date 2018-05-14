371ab8852e63318606645fabb3d7f52b16cff31f
Jamie Cruwys <me@jamiecruwys.co.uk>
2018-05-14 23:37:35 +0100

    One apple basket test now passes

--- app/src/main/java/uk/co/jamiecruwys/cart/Basket.java
+++ app/src/main/java/uk/co/jamiecruwys/cart/Basket.java
@@ -3,7 +3,13 @@ package uk.co.jamiecruwys.cart;
 import java.math.BigDecimal;
 
 public class Basket {
+    private BigDecimal total = new BigDecimal(0.00);
+
+    public void add(Apple apple) {
+        total = total.add(apple.getPrice());
+    }
+
     public BigDecimal getTotal() {
-        return new BigDecimal(0.00);
+        return total;
     }
 }
