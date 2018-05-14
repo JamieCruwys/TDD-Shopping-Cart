96ae7b4f88b95f624780a42a7736f112f123a6eb
Jamie Cruwys <me@jamiecruwys.co.uk>
2018-05-14 23:45:44 +0100

    Addressed BigDecimal scaling issue

--- app/src/main/java/uk/co/jamiecruwys/cart/Basket.java
+++ app/src/main/java/uk/co/jamiecruwys/cart/Basket.java
@@ -1,6 +1,7 @@
 package uk.co.jamiecruwys.cart;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Basket {
     private BigDecimal total = new BigDecimal(0.00);
@@ -10,6 +11,6 @@ public class Basket {
     }
 
     public BigDecimal getTotal() {
-        return total;
+        return total.setScale(2, RoundingMode.CEILING);
     }
 }
--- app/src/test/java/uk/co/jamiecruwys/cart/BasketUnitTest.java
+++ app/src/test/java/uk/co/jamiecruwys/cart/BasketUnitTest.java
@@ -3,6 +3,7 @@ package uk.co.jamiecruwys.cart;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import static org.junit.Assert.assertEquals;
 
@@ -10,7 +11,7 @@ public class BasketUnitTest {
     @Test
     public void empty_basket_value_is_0() {
         BigDecimal actual = new Basket().getTotal();
-        assertEquals(new BigDecimal(0.00), actual);
+        assertEquals(new BigDecimal(0.00).setScale(2, RoundingMode.CEILING), actual);
     }
 
     @Test
@@ -18,6 +19,15 @@ public class BasketUnitTest {
         Basket basket = new Basket();
         basket.add(new Apple());
         BigDecimal actual = basket.getTotal();
-        assertEquals(new BigDecimal(0.60), actual);
+        assertEquals(new BigDecimal(0.60).setScale(2, RoundingMode.CEILING), actual);
+    }
+
+    @Test
+    public void two_apples_basket_value_is_correct() {
+        Basket basket = new Basket();
+        basket.add(new Apple());
+        basket.add(new Apple());
+        BigDecimal actual = basket.getTotal();
+        assertEquals(new BigDecimal(1.20).setScale(2, RoundingMode.CEILING), actual);
     }
 }
