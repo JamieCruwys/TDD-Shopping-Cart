f4e1783e7900865cd81d25e45f1403fcb971c4fe
Jamie Cruwys <me@jamiecruwys.co.uk>
2018-05-14 23:16:31 +0100

    Fixed syntax to match Java. I'm still in Kotlin mode...

--- app/src/test/java/uk/co/jamiecruwys/cart/ApplesUnitTest.java
+++ app/src/test/java/uk/co/jamiecruwys/cart/ApplesUnitTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.*;
 public class ApplesUnitTest {
     @Test
     public void apple_price_is_correct() {
-        BigDecimal actual = Apple().getPrice();
+        BigDecimal actual = new Apple().getPrice();
         assertEquals(new BigDecimal(0.6), actual);
     }
 }
