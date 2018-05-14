3a855a8d8cc3fa75849d7219ac90b7fad10d7574
Jamie Cruwys <me@jamiecruwys.co.uk>
2018-05-14 23:52:47 +0100

    Changed add method signature to take a Product instead. Tests now pass

--- app/src/main/java/uk/co/jamiecruwys/cart/Basket.java
+++ app/src/main/java/uk/co/jamiecruwys/cart/Basket.java
@@ -3,11 +3,13 @@ package uk.co.jamiecruwys.cart;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+import uk.co.jamiecruwys.cart.products.Product;
+
 public class Basket {
     private BigDecimal total = new BigDecimal(0.00);
 
-    public void add(Apple apple) {
-        total = total.add(apple.getPrice());
+    public void add(Product product) {
+        total = total.add(product.getPrice());
     }
 
     public BigDecimal getTotal() {
