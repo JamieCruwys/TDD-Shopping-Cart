e0a5d0a06fe3baeb435313bec926340024878a69
Jamie Cruwys <me@jamiecruwys.co.uk>
2018-05-15 00:00:42 +0100

    Refactored basket. Tests pass

--- app/src/main/java/uk/co/jamiecruwys/cart/Basket.java
+++ app/src/main/java/uk/co/jamiecruwys/cart/Basket.java
@@ -2,17 +2,22 @@ package uk.co.jamiecruwys.cart;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
 
 import uk.co.jamiecruwys.cart.products.Product;
 
 public class Basket {
-    private BigDecimal total = new BigDecimal(0.00);
+    private ArrayList<Product> products = new ArrayList<>();
 
     public void add(Product product) {
-        total = total.add(product.getPrice());
+        products.add(product);
     }
 
     public BigDecimal getTotal() {
+        BigDecimal total = new BigDecimal(0.00);
+        for (Product product : products) {
+            total = total.add(product.getPrice());
+        }
         return total.setScale(2, RoundingMode.HALF_UP);
     }
 }
