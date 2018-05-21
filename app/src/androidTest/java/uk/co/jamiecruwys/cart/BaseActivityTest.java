db30e4e68c0346416bf4ddd2bd3da60c2f6ab32d
Jamie Cruwys <me@jamiecruwys.co.uk>
2018-05-21 21:28:08 +0100

    Added base activity test

---
+++ app/src/androidTest/java/uk/co/jamiecruwys/cart/BaseActivityTest.java
@@ -0,0 +1,17 @@
+package uk.co.jamiecruwys.cart;
+
+import android.app.Activity;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+
+/**
+ * Base tests for testing activities
+ * @param <T> representing the {@link android.app.Activity} you wish to test
+ */
+abstract public class BaseActivityTest<T extends Activity> {
+	@Rule
+	public ActivityTestRule<T> test = new ActivityTestRule<>(provideTestActivity());
+
+	abstract Class<T> provideTestActivity();
+}
